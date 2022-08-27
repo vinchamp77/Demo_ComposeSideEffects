18160bae27c9bee0490dd236dca2c802116cebe1
vinchamp77 <vinchamp77@gmail.com>
2022-08-27 22:38:50 +1000

    Add comment

--- app/src/main/java/vtsen/hashnode/dev/composesideeffectsdemo/ui/demo/RememberUpdatedState.kt
+++ app/src/main/java/vtsen/hashnode/dev/composesideeffectsdemo/ui/demo/RememberUpdatedState.kt
@@ -11,9 +11,17 @@ fun RememberUpdatedStated(value: String) {
     val rememberMutableStateValue by remember { mutableStateOf(value) }
     val rememberUpdatedStateValue by rememberUpdatedState(value)
 
+    // (2) LaunchedEffect is skipped during the second recomposition
+    //	   when value is changed/updated
     LaunchedEffect(true) {
         while(true) {
+            // (1) let's assume value is updated with a new value within
+            //     1 second delay
             delay(1000)
+
+            // (3) value is the initial value when LaunchedEffect is first called
+            // (4) rememberMutableStateValue is the initial value from first composition
+            // (5) rememberUpdatedStateValue is the new value from second recomposition
             Log.d("RememberUpdatedStated",
                 "[Old]: value: $value " +
                 "[Old]:rememberValue: $rememberValue " +
